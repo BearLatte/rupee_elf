fd96f4061751a17c011a61df4930d07fcf0ac0cf
Tim <Tim20200212@gmail.com>
2023-08-28 10:22:09 +0800

    提测,修改build版本

--- lib/application.dart
+++ lib/application.dart
@@ -18,6 +18,7 @@ class Application extends StatelessWidget {
     RouterManager.defineRoutes(router);
     initAdjust();
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       onGenerateRoute: router.generator,
       theme: ThemeData(
         colorScheme:
--- lib/component/login/index.dart
+++ lib/component/login/index.dart
@@ -1,4 +1,3 @@
-import 'package:facebook_app_events/facebook_app_events.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
